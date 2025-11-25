4e3c406f6f8d7db14f4726007efa3b8d14189d50
NanaVan <van4wang@hotmail.com>
2025-11-25 15:21:07 +0800

    add function to load the date from puyuan device(under testing)

--- preprocessing.py
+++ preprocessing.py
@@ -5,8 +5,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 from nptdms import TdmsFile
+from typing import List, Dict, Tuple
 from datetime import datetime, timedelta
-import json, sys, os.path
+import json, sys, os.path, struct, warnings
+
+def read_sua_header128(header_data):
+    '''
+    read SUA format file with 128-byte header
+    '''
+    if len(header_data) < 128:
+        raise ValueError("Header data must be at least 128 bytes")
+    fields = struct.unpack('<IIIBBHHHIBIBH32s16I', header_data[:128])
+    header = {
+        'magic': fields[0],         # 0x01DCEF18
+        'prt_count': fields[1],
+        'packet_len': fields[2],
+        'pack_info': fields[3],
+        'data_width': fields[4],
+        'interleave': fields[5],
+        'channel_count': fields[6],
+        'channel_id': fields[7],
+        'prt_width': fields[8],
+        'data_type': fields[9],
+        'total_len': fields[10],
+        'reserved1': fields[11],
+        'reserved2': fields[12],
+        'comment': fields[13].decode(errors='ignore').strip('\x00'),
+    }
+    for i in range(16):
+        header[f'ext_field_{i}'] = fields[14 + i]
+    return header
+data_type_map = {3: np.int8, 5: np.int16, 6: np.int16} # 6: QI pairing, each for int16
+
 
 class Preprocessing(object):
     '''
@@ -33,8 +63,11 @@ class Preprocessing(object):
         elif self.fname[-4:].lower() == "tdms":
             self.file_format = "tdms"
             self.extract_tdms()
+        elif self.fname[-4:].lower() == "data":
+            self.file_format = "data"
+            self.extract_data()
         else:
-            print("Error: unrecognized file format!\nOnly 'wv' or 'tiq' file can be accepted.")
+            print("Error: unrecognized file format!\nOnly 'wv' (from R&S devices) or 'tiq' (from Tek devices) or 'tdms' (from NI devices) or 'data' (from puyuan devices) file can be accepted.")
             sys.exit()
         if verbose:
             self.display()
@@ -66,6 +99,8 @@ class Preprocessing(object):
             root = ET.fromstring(tiq.read(self.n_offset))
         def get_value(key):
             return next(root[0][0].iter(prefix + key)).text
+        # since the analyzers are in the same timezone, we use the datetime in a simple way.
+        warnings.filterwarnings("ignore", message="no explict representation of timezones available for np.datetime64")
         self.date_time = np.datetime64(get_value("DateTime"))
         self.data_format = np.dtype(get_value("NumberFormat").lower()).newbyteorder(get_value("Endian").lower())
         self.ref_level = float(get_value("ReferenceLevel")) # dBm
@@ -91,6 +126,22 @@ class Preprocessing(object):
             self.n_sample = 0
             for chunk in tdms.data_chunks():
                 self.n_sample += len(chunk['RecordData']['I'])
+
+    def extract_data(self):
+        '''
+        extract the metadata from the .data file collected by puyuan device
+        '''
+        self.n_offset = 128 
+        with open('/'.join((self.fpath, self.fname)), 'rb') as f:
+            header_data = read_sua_header128(f.read(self.n_offset))
+        self.packet_len = header_data['packet_len']
+        self.data_format = data_type_map.get(header_data['data_type'])
+        self.date_time = ''
+        self.n_sample = os.path.getsize('/'.join((self.fpath, self.fname))) // self.packet_len * ((self.packet_len - self.n_offset) // self.data_format().itemsize // 2) 
+        self.sampling_rate = 31.25e6
+        self.span = self.sampling_rate * 0.8
+        self.gain = 1.0
+        self.center_frequency = 243e6
 
     def display(self):
         '''
@@ -137,6 +188,16 @@ class Preprocessing(object):
         elif self.file_format == "tiq": # for tiq
             tiq = np.memmap('/'.join((self.fpath, self.fname)), dtype=self.data_format, offset=self.n_offset+offset*2*self.data_format.itemsize, mode='r')
             data = tiq[:2*size*decimating_factor].reshape(size,2*decimating_factor)[:,:2].flatten().astype(float).view(complex) * self.scaling # V
+        elif self.file_format == "data":  # for puyuan SUA data
+            # amount of IQ pairs for each packet
+            packet_size_for_data = (self.packet_len - self.n_offset) // 2 // self.data_format().itemsize
+            packet_start = offset // packet_size_for_data
+            packet_count = (offset + size) // packet_size_for_data + 1 - packet_start
+            with open('/'.join((self.fpath, self.fname)), 'rb') as sua:
+                sua.seek(packet_start*self.packet_len)
+                data_buffer = np.frombuffer(sua.read(packet_count*self.packet_len), dtype=self.data_format)
+            actual_packet_num = data_buffer.size // (self.packet_len // self.data_format().itemsize)
+            data = np.hstack(data_buffer.reshape(actual_packet_num, -1)[:, self.n_offset//self.data_format().itemsize:])[2*(offset-packet_start*packet_size_for_data):2*(size+offset-packet_start*packet_size_for_data)].reshape(size,2)[:,:2].flatten().astype(float).view(complex) * self.gain
         else: # for tdms
             def data_return(_chunk, _offset, _total_size):
                 if _offset >= len(_chunk):
